ee96cb4826a779918d758b830f4854285129e5de
Tom Seidel <hi@tomseidel.com>
2025-06-12 22:50:21 +0200

    feat: add variables and types experiment

---
+++ variables-types/variables.py
@@ -0,0 +1,30 @@
+surName = "Tom"
+
+x = 1
+y = 2
+z = 3.14159
+
+# Casting
+lastName = str("Seidel")
+
+x1 = int(1)
+x2 = int(2)
+x3 = float(3.14159)
+
+print(surName, lastName)
+print(x, y, z)
+print(x+x3-x2)
+
+print (type(surName))
+print (type(lastName))
+print (type(x))
+print (type(x1))
+print (type(x2))
+print (type(x3))
+
+# Overwrite
+x = "one"
+y = "two"
+z = "three"
+
+print(x, y, z)
