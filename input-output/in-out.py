1df1a687f506bed8cdc6d03435f64389ab9ef8d4
Tom Seidel <hi@tomseidel.com>
2025-06-12 23:08:29 +0200

    feat: add input / output program

---
+++ input-output/in-out.py
@@ -0,0 +1,14 @@
+# Input and Output in Python
+user_name = input("Enter your name: ")
+print("Hello, " + user_name)
+
+user_age = input("Enter your age: ")
+print("You are " + user_age + " years old.")
+
+user_height = input("Enter your height: ")
+print("Your height is " + user_height + " cm.")
+
+# Calculation
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("Sum:", x + y)
