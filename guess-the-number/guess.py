fc82e72f0d5c256529872812143bd70db3308a4e
Tom Seidel <hi@tomseidel.com>
2025-07-03 10:44:22 +0200

    feat: check if number is too high or too low in a loop

--- guess-the-number/guess.py
+++ guess-the-number/guess.py
@@ -1,14 +1,19 @@
 import random
 
 print("I'm thinking of a number between 1 and 10.")
-#secret random number
+# secret random number
 secret = random.randint(1,10)
 
-#user input
+# user input
 guess = int(input("What's your guess? "))
 
-#check if guess is correct
+# check if guess is too high or too low in a loop
+while guess != secret:
+    if guess > secret:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
+    guess = int(input("What's your guess? "))
+    # if guess is correct
 if guess == secret:
-    print("You got it!")
-else:
-    print("Nope. The number I was thinking of was", secret)
+    print("You got it!") 
