0af46a307dcc460f62a0fda477a536aee9ea7325
tom-seidel <hi@tomseidel.com>
2025-07-07 11:22:31 +0200

    feat: add exception and other checks

--- guess-the-number/guess.py
+++ guess-the-number/guess.py
@@ -3,17 +3,23 @@ import random
 print("I'm thinking of a number between 1 and 10.")
 # secret random number
 secret = random.randint(1,10)
-
-# user input
-guess = int(input("What's your guess? "))
-
-# check if guess is too high or too low in a loop
-while guess != secret:
+ 
+while True:
+    # check if guess is a number
+    try:
+        guess = int(input("What's your guess? "))
+    except ValueError:
+        print("That's not a valid number! Please try again.")
+        continue
+    #check if guess is between 1 and 10
+    if guess < 1 or guess > 10:
+        print("The number must be between 1 and 10! Please try again.")
+        continue
+    # number is too high or too low
     if guess > secret:
         print("Too high! Try again.")
-    else:
+    elif guess < secret:
         print("Too low! Try again.")
-    guess = int(input("What's your guess? "))
-    # if guess is correct
-if guess == secret:
-    print("You got it!") 
+    else:
+        print("You got it!") # guess is correct
+        break
